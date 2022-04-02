e9f603ec22365b3262fa5ff69341d3bbdc70d568
unknown <tamiloreolaniyi@gmail.com>
2022-04-02 20:54:12 +0100

    project list

--- src/Components/dashboard/Dashboard.jsx
+++ src/Components/dashboard/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Notify from './Notify'
+import ProjectList from '../projects/ProjectList'
 
 function Dashboard() {
   return (
--- src/Components/projects/ProjectList.jsx
+++ src/Components/projects/ProjectList.jsx
@@ -3,7 +3,29 @@ import React from 'react'
 function ProjectList() {
   return (
     <div className='project-list section'>
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text-darken-3">
+                <span className="card-title">Project Title</span>
+                <p>Posted by the Net Ninja</p>
+                <p className="grey-text">3rd Sept, 2am</p>
+            </div>
+        </div>
 
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text-darken-3">
+                <span className="card-title">Project Title</span>
+                <p>Posted by the Net Ninja</p>
+                <p className="grey-text">3rd Sept, 2am</p>
+            </div>
+        </div>
+
+        <div className="card z-depth-0 project-summary">
+            <div className="card-content grey-text-darken-3">
+                <span className="card-title">Project Title</span>
+                <p>Posted by the Net Ninja</p>
+                <p className="grey-text">3rd Sept, 2am</p>
+            </div>
+        </div>
     </div>
   )
 }
