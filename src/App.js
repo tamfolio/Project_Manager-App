5cdd38be72ba711547809605a0d39ba3e8bf3daf
unknown <tamiloreolaniyi@gmail.com>
2022-04-02 06:06:23 +0100

    materialize css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import {BrowserRouter} from 'react-router-dom';
+import Navbar from './Components/layout/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
-     <h1>Update Boys</h1>
+     <Navbar/> 
     </div>
     </BrowserRouter>
   );
