43612fb0f109e386f266f202b270e8f20f89f13c
unknown <tamiloreolaniyi@gmail.com>
2022-04-02 21:24:59 +0100

    project details 2

--- src/Components/projects/ProjectDetails.jsx
+++ src/Components/projects/ProjectDetails.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-function ProjectDetails() {
+function ProjectDetails(props) {
+    const id = props.match.params.id;
   return (
     <div className='container section project-details'>
         <div className="card z-depth-0">
             <div className="card-content">
-                <div className="card-title">Project Title</div>
+                <div className="card-title">Project Title - {id}</div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum libero harum labore impedit, ipsa sit accusamus quasi molestiae deleniti eum, molestias reiciendis deserunt magni aperiam sapiente illo eius vitae eligendi?</p>
             </div>
             <div className="card-action grey lighten-4 grey-text">
