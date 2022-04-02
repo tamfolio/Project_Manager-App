06033314f9ddef40f1ed226c6eb99446b868acb5
unknown <tamiloreolaniyi@gmail.com>
2022-04-02 21:18:55 +0100

    project details

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Navbar from './Components/layout/Navbar';
 import Dashboard from './Components/dashboard/Dashboard';
+import ProjectDetails from './Components/projects/ProjectDetails';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
     <div className="App">
      <Navbar/>
      <Switch>
-       <Route path='/' component={Dashboard}/>
+       <Route exact path='/' component={Dashboard}/>
+       <Route path='/project/:id' component={ProjectDetails}/>
      </Switch>
     </div>
     </BrowserRouter>
---
+++ src/Components/projects/ProjectDetails.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ProjectDetails() {
+  return (
+    <div className='container section project-details'>
+        <div className="card z-depth-0">
+            <div className="card-content">
+                <div className="card-title">Project Title</div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum libero harum labore impedit, ipsa sit accusamus quasi molestiae deleniti eum, molestias reiciendis deserunt magni aperiam sapiente illo eius vitae eligendi?</p>
+            </div>
+            <div className="card-action grey lighten-4 grey-text">
+                <div>Posted by Tamilore</div>
+                <div>2nd September, 2am</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectDetails
