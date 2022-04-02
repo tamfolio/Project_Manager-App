aa44a3c51820b7572d3aa1cfee64d04ef883c905
unknown <tamiloreolaniyi@gmail.com>
2022-04-02 22:10:06 +0100

    Sign Up form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Components/layout/Navbar';
 import Dashboard from './Components/dashboard/Dashboard';
 import ProjectDetails from './Components/projects/ProjectDetails';
 import SignIn from './Components/auth/SignIn';
+import SignUp from './Components/auth/SignUp';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
        <Route exact path='/' component={Dashboard}/>
        <Route path='/project/:id' component={ProjectDetails}/>
        <Route path='/signin' component={SignIn}/>
+       <Route path='/signup' component={SignUp}/>
      </Switch>
     </div>
     </BrowserRouter>
